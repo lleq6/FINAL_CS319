34cc7cb190ffa13c62362575d71ce2f7c72c6a62
__lleq <ball_090573@hotmail.com>
2024-12-21 02:17:24 +0700

    final update

--- src/app/api/admin/uploadImage/addImage.ts
+++ src/app/api/admin/uploadImage/addImage.ts
@@ -1,23 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
 import path from "path";
+import fs from "fs";
 import { writeFile } from "fs/promises";
 import { NextApiRequest } from "next";
 
-export const uploadImage = async (file : any) => {
+export const uploadImage = async (file: any) => {
   if (!file) {
-    return false
+    return false;
   }
-
   const buffer = Buffer.from(await file.arrayBuffer());
   const filename = Date.now() + file.name.replaceAll(" ", "_");
+  const root = `public`;
+  const dir = `products`;
+  const location = `${dir}/${filename}`;
   try {
-    await writeFile(
-      path.join(process.cwd(), `public/products/${filename}`),
-      buffer
-    );
-    return {path : `/products/${filename}`}
+    if (!fs.existsSync(`${root}/${dir}`)) {
+      fs.mkdirSync(`${root}/${dir}`, { recursive: true });
+    }
+    await writeFile(path.join(process.cwd(), `${root}/${location}`), buffer);
+    return { path: `/${location}` };
   } catch (error) {
     console.log("Error occured ", error);
-    return false
+    return false;
   }
 };
